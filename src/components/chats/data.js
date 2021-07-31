638efd0e9e204ffb5004f8d3b7b7d6f4437bfb1b
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-07-31 17:29:11 +0300

    fix typo

--- src/components/chats/data.js
+++ src/components/chats/data.js
@@ -39,7 +39,7 @@ export default [
   {
     id: 'osajnd809jo1',
     name: 'Jamie Carr',
-    preview: 'Den why have awesome inputs? Hmmm?',
+    preview: 'Den why do you have so awesome inputs? Hmmm?',
     at: Date.now(),
     avatar:
       'https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=3759e09a5b9fbe53088b23c615b6312e',
